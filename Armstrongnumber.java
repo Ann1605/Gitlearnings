84ad2849cd53504a191c605e626ecae9c4138a68
Mangai-1308 <110008877+Mangai-1308@users.noreply.github.com>
2023-06-22 02:03:02 +0530

    New File Uploaded

---
+++ Armstrongnumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Armstrongnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int num = sc.nextInt();
+        int n = num;
+        int res =0;
+        while(n!=0){
+            int rem = n%10;
+            res += Math.pow(rem,3);
+            n = n/10;
+        }
+        if(res == num)
+            System.out.println("The given number is armstrong number");
+        else
+            System.out.println("The given number is not armstrong number");
+    }
+}
